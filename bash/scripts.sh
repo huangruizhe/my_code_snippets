ffa3eb161ecb6bb207ccadc88038d39f3b591b01
huangruizhe <eraser567@163.com>
2022-07-04 15:21:34 -0400

    Update scripts.sh
    example of using bash array of strings

---
+++ bash/scripts.sh
@@ -0,0 +1,10 @@
+# example of using bash array of strings
+
+text=()
+text+=("/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/data/train/text")
+text+=("/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/data/train_dev/text")
+text+=("/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/data/std2006_dev/text")
+text+=("/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/data/std2006_eval/text")
+wc $(printf " %s" "${text[@]}")
+inputfiles=$(printf " -i %s " "${text[@]}")
+python ... inputfiles ...
