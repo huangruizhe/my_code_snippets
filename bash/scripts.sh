eafcef7256e736f1932d2a6c8fd4380fca854ea6
huangruizhe <eraser567@163.com>
2022-07-04 15:25:34 -0400

    Update scripts.sh

--- bash/scripts.sh
+++ bash/scripts.sh
@@ -1,4 +1,6 @@
+########################################################
 # example of using bash array of strings
+########################################################
 
 text=()
 text+=("/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/data/train/text")
@@ -6,5 +8,5 @@ text+=("/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/data/train_dev/text")
 text+=("/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/data/std2006_dev/text")
 text+=("/export/fs04/a12/rhuang/kws/kws_exp/shay/s5c/data/std2006_eval/text")
 wc $(printf " %s" "${text[@]}")
-inputfiles=$(printf " -i %s " "${text[@]}")
+inputfiles=$(printf " -i %s " "${text[@]}")    # be careful of the space before -i
 python ... inputfiles ...
